8c68c2373e404d7fb485e12f951d365a89d3babf
JimWest <j1m_w3st@web.de>
2017-04-25 18:11:53 +0200

    Added gitignore file + basic project folder structure

---
+++ src/controller/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World from controller!" << std::endl;
+    return 0;
+}
---
+++ src/gui/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World from gui!" << std::endl;
+    return 0;
+}
