8418f88adc827bbc9cdfc49f8ed63175037a0d0b
Lisa Winter <ljb.win10@gmail.com>
2017-05-15 21:21:36 +0200

    Added ChessColorEnum

---
+++ src/shared_lib/ChessColor.h
@@ -0,0 +1,15 @@
+//
+// Created by lisa on 5/15/17.
+//
+
+#ifndef CHESS_AR_CHESSCOLOR_H
+#define CHESS_AR_CHESSCOLOR_H
+
+enum ChessColor {
+    WHITE,
+    BLACK,
+    NONE
+};
+
+
+#endif //CHESS_AR_CHESSCOLOR_H
--- src/shared_lib/ChessFigure.h
+++ src/shared_lib/ChessFigure.h
@@ -5,6 +5,8 @@
 #ifndef CHESS_AR_CHESSFIGURE_H
 #define CHESS_AR_CHESSFIGURE_H
 
+#include "ChessColor.h"
+
 enum FigureType {
     EMPTY,
     KING,
@@ -17,6 +19,7 @@ enum FigureType {
 
 struct ChessFigure {
     FigureType figure_type = EMPTY;
+    ChessColor color       = NONE;
 };
 
 #endif //CHESS_AR_CHESSFIGURE_H
