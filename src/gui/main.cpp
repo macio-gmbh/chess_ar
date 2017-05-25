9c52e7a3091a1152a6cf8ebda2a36aa6128206ad
Lisa Winter <ljb.win10@gmail.com>
2017-05-25 21:36:14 +0200

    Added simple output for terminal. Message Brocker still missing

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,6 +1,69 @@
 #include <iostream>
+#include <cctype>
+#include <boost/algorithm/string.hpp>
+#include <boost/container/vector.hpp>
+#include <boost/lexical_cast.hpp>
+
+using namespace boost::container;
+
+static void boardPrinter(std::string fen) {
+
+    std::cout << " ############  Current game status is:  ############" << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    vector<std::string> fenList;
+    boost::split(fenList, fen, boost::is_any_of(" "));
+
+    std::cout << "         | ";
+    for (std::string::iterator it = fenList[0].begin(); it != fenList[0].end(); ++it) {
+
+        if ((*it) == '/') {
+            std::cout << std::endl;
+            std::cout << "         | ";
+        } else if (isdigit(*it)) {
+            std::stringstream ss;
+            ss << *it;
+            int emptySpaces = 0;
+            ss>>emptySpaces;
+            for (int i = 0; i < emptySpaces; i++) {
+                std::cout << "0 | ";
+            }
+        } else {
+            std::cout << *it << " | ";
+        }
+    }
+
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << "Current player to move is: " ;
+
+    if (fenList.size() > 1) {
+        switch (fenList[1].at(0)) {
+            case 'w':
+                std::cout << " WHITE " << std::endl;
+                break;
+            case 'b':
+                std::cout << " BLACK " << std::endl;
+                break;
+            default:
+                std::cout << "unknown" << std::endl;
+
+        }
+    }
+
+    std::cout << "Next recommened move is:  TODO" ;
+
+
+
+}
 
 int main() {
     std::cout << "Hello, World from gui!" << std::endl;
+    std::cout << "--------------- This is a test output---------------" << std::endl;
+    std::string testBoard = "rnbqkbnr/pp1ppppp/8/2p5/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    boardPrinter(testBoard);
     return 0;
 }
+
+
