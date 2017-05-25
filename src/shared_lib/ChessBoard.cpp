6e91458150e250cc8bc09e09cb85945c1d9a6ebd
Lisa Winter <ljb.win10@gmail.com>
2017-05-25 17:12:22 +0200

    Fixed Constructor

--- src/shared_lib/ChessBoard.cpp
+++ src/shared_lib/ChessBoard.cpp
@@ -8,121 +8,138 @@
 ChessBoard::ChessBoard(std::string &fen) {
     char currentLetter;
     vector<std::string> fenList;
-    boost::split(fenList, fen,boost::is_any_of(" "));
+    boost::split(fenList, fen, boost::is_any_of(" "));
 
-    uint8_t runner = 0;
+    uint8_t iChar = 0;
+    uint8_t iArray = 0;
 
-    while(runner < 64) {
-        currentLetter = fenList[0].at(runner);
+    while (iArray < 64) {
+        currentLetter = fenList[0].at(iChar);
+        //point to nextChar
+        iChar += 1;
 
         switch (currentLetter) {
             case 'p' : {
-                board[runner].color = ChessColor ::BLACK;
-                board[runner].figure_type = FigureType::PAWN;
+                board[iArray].color = ChessColor::BLACK;
+                board[iArray].figure_type = FigureType::PAWN;
+                iArray += 1;
                 break;
             }
             case 'r' : {
-                board[runner].color = ChessColor ::BLACK;
-                board[runner].figure_type = FigureType::ROOK;
+                board[iArray].color = ChessColor::BLACK;
+                board[iArray].figure_type = FigureType::ROOK;
+                iArray += 1;
                 break;
             }
             case 'n' : {
-                board[runner].color = ChessColor ::BLACK;
-                board[runner].figure_type = FigureType::KNIGHT;
+                board[iArray].color = ChessColor::BLACK;
+                board[iArray].figure_type = FigureType::KNIGHT;
+                iArray += 1;
                 break;
             }
             case 'b' : {
-                board[runner].color = ChessColor ::BLACK;
-                board[runner].figure_type = FigureType::BISHOP;
+                board[iArray].color = ChessColor::BLACK;
+                board[iArray].figure_type = FigureType::BISHOP;
+                iArray += 1;
                 break;
             }
             case 'q' : {
-                board[runner].color = ChessColor ::BLACK;
-                board[runner].figure_type = FigureType::QUEEN;
+                board[iArray].color = ChessColor::BLACK;
+                board[iArray].figure_type = FigureType::QUEEN;
+                iArray += 1;
                 break;
             }
             case 'k' : {
-                board[runner].color = ChessColor ::BLACK;
-                board[runner].figure_type = FigureType::KING;
+                board[iArray].color = ChessColor::BLACK;
+                board[iArray].figure_type = FigureType::KING;
+                iArray += 1;
                 break;
             }
             case 'P' : {
-                board[runner].color = ChessColor ::WHITE;
-                board[runner].figure_type = FigureType::PAWN;
+                board[iArray].color = ChessColor::WHITE;
+                board[iArray].figure_type = FigureType::PAWN;
+                iArray += 1;
                 break;
             }
             case 'R' : {
-                board[runner].color = ChessColor ::WHITE;
-                board[runner].figure_type = FigureType::ROOK;
+                board[iArray].color = ChessColor::WHITE;
+                board[iArray].figure_type = FigureType::ROOK;
+                iArray += 1;
                 break;
             }
             case 'N' : {
-                board[runner].color = ChessColor ::WHITE;
-                board[runner].figure_type = FigureType::KNIGHT;
+                board[iArray].color = ChessColor::WHITE;
+                board[iArray].figure_type = FigureType::KNIGHT;
+                iArray += 1;
                 break;
             }
             case 'B' : {
-                board[runner].color = ChessColor ::WHITE;
-                board[runner].figure_type = FigureType::BISHOP;
+                board[iArray].color = ChessColor::WHITE;
+                board[iArray].figure_type = FigureType::BISHOP;
+                iArray += 1;
                 break;
             }
             case 'Q' : {
-                board[runner].color = ChessColor ::WHITE;
-                board[runner].figure_type = FigureType::QUEEN;
+                board[iArray].color = ChessColor::WHITE;
+                board[iArray].figure_type = FigureType::QUEEN;
+                iArray += 1;
                 break;
             }
             case 'K' : {
-                board[runner].color = ChessColor ::WHITE;
-                board[runner].figure_type = FigureType::KING;
+                board[iArray].color = ChessColor::WHITE;
+                board[iArray].figure_type = FigureType::KING;
+                iArray += 1;
                 break;
             }
             case '/' : {
-                runner--;
                 break;
             }
             case '1' : {
-                emtyTileSetter(1, runner);
+                emtyTileSetter(1, iArray);
                 break;
             }
             case '2' : {
-                emtyTileSetter(2, runner);
+                emtyTileSetter(2, iArray);
                 break;
             }
             case '3' : {
-                emtyTileSetter(3, runner);
+                emtyTileSetter(3, iArray);
                 break;
             }
             case '4' : {
-                emtyTileSetter(4, runner);
+                emtyTileSetter(4, iArray);
                 break;
             }
             case '5' : {
-                emtyTileSetter(5, runner);
+                emtyTileSetter(5, iArray);
                 break;
             }
             case '6' : {
-                emtyTileSetter(6, runner);
+                emtyTileSetter(6, iArray);
                 break;
             }
             case '7' : {
-                emtyTileSetter(7, runner);
+                emtyTileSetter(7, iArray);
                 break;
             }
             case '8' : {
-                emtyTileSetter(8, runner);
+                emtyTileSetter(8, iArray);
                 break;
             }
             default:
                 break;
         }
+
     }
 
-    if(fenList.size() > 1) {
-        switch(fenList[1].at(0)) {
+    if (fenList.size() > 1) {
+        switch (fenList[1].at(0)) {
             case 'w':
                 currentMove = ChessColor::WHITE;
+                break;
             case 'b':
                 currentMove = ChessColor::BLACK;
+                break;
             default:
                 currentMove = ChessColor::NONE;
 
