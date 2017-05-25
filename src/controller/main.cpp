d896df1e094bbbf2048ba9daad517120bb4b35a5
Lisa Winter <ljb.win10@gmail.com>
2017-05-25 17:11:22 +0200

    some minor Tests, will be moved to gtest eventually

--- src/controller/main.cpp
+++ src/controller/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include "../shared_lib/ChessBoard.h"
+
 
 int main() {
-    std::cout << "Hello, World from controller!" << std::endl;
+    std::cout << "Hello, World from controller! Initialising ..." << std::endl;
+    std::string initialBoard = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    std::cout<< "Initial Setup Test: " << initialBoard <<std::endl;
+    //ChessBoard currentBoard = ChessBoard(initialBoard);
+    //std::string res = currentBoard.toString();
+    //std::cout<< "Initial to_string Test : " << res <<std::endl;
+
+
+
+    std::string testBoard = "rnbqkbnr/pp1ppppp/8/2p5/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    std::cout<< "Initial Setup Test: " << testBoard <<std::endl;
+    ChessBoard currentBoard = ChessBoard(testBoard);
+    std::string res2 = currentBoard.toString();
+    std::cout<< "Initial to_string Test : " << res2 <<std::endl;
+
     return 0;
 }
